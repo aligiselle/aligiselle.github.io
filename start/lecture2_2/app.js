cd11fc2a3ef7c0e63e819bfbc6d25b8866c8f366
aligiselle <89480515+aligiselle@users.noreply.github.com>
2025-05-28 12:32:31 -0400

    Update app.js

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -6,19 +6,26 @@ class App{
 				const container = document.createElement( 'div' );
 				document.body.appendChild( container );
 
-				this.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
+				this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
 				this.camera.position.set(0,0,4);
 
 				this.scene = new THREE.Scene();
-				this.scene.background = new THREE.Color(0xaaaaaa);
+				this.scene.background = new THREE.Color( 0xaaaaaa );
 
-				this.renderer = new THREE.WebGLRenderer( { antialias:true });
+				this.renderer = new THREE.WebGLRenderer( { antialias: true });
 				this.renderer.setPixelRatio( window.devicePixelRatio );
 				this.renderer.setSize( window.innerWidth, window.innerHeight );
 				container.appendChild( this.renderer.domElement );
 
 				this.renderer.setAnimationLoop( this.render.bind(this) );
         
+				const geometry = new THREE.BoxBufferGeometry();
+				const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+
+				this.mesh = new THREE.Mesh(geometry, material);
+
+				this.scene.add (this.mesh);
+
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
@@ -27,7 +34,7 @@ class App{
     }
     
 	render( ) {  
-        
+        this.renderer.render(this.scene, this.camera);
     }
 }
 
