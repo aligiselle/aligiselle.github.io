d07fc6c70d190f7ea45a5a9ae49a34bae8e62f0f
aligiselle <89480515+aligiselle@users.noreply.github.com>
2025-06-09 09:41:08 -0400

    Update main.js

--- TITL/main.js
+++ TITL/main.js
@@ -5,6 +5,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -19,4 +20,3 @@ function animate() {
   cube.rotation.y += 0.01;
   renderer.render( scene, camera );
 }
-renderer.setAnimationLoop( animate );
