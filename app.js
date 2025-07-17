0ae1b145da2bc24834bfd9cc9a5dc2f743134cb4
aligiselle <89480515+aligiselle@users.noreply.github.com>
2025-07-17 12:43:08 -0400

    video

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const arrow = document.getElementById("arrow");
 arrow.src="/assets/arrow.png";
  
 window.addEventListener("click", function(){
-  video.play();
-  /** 
-  if (video && !video.paused){
+   /** video.play();*/
+
+  if (!video.paused){
     video.pause();
   } else {
     video.play();
-  } */
+  } 
 });
 
