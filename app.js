f4f3fb161199a0396bb24e81f4af35083890ad9e
aligiselle <89480515+aligiselle@users.noreply.github.com>
2025-07-17 12:14:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const arrow = document.getElementById("arrow");
 arrow.src="/assets/arrow.png";
  
 window.addEventListener("click", function(){
+  video.play();
+  /** 
   if (video && !video.paused){
     video.pause();
   } else {
     video.play();
-  } 
+  } */
 });
 
